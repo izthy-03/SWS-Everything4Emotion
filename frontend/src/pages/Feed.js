d7b770d06cac91fff95d95a54b3284d4915d2bd3
izthy-512 <908945962@qq.com>
2023-07-13 19:59:26 +0800

    no submit no refresh

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -5,29 +5,37 @@ import { get, post, put } from "../utilities";
 const Feed = () => {
 
   const [res, setRes] = useState({});
-  const [req, setReq] = useState({});
 
   useEffect(() => {
 
+    let bodyStr = sessionStorage.getItem("request");
     let body = JSON.parse(sessionStorage.getItem("request"));
-    console.log("lastReq:\n" + req);
-    console.log("nowReq:\n" + body);
 
-    if (body !== req) {
-      setReq(body);
+    let lastReqStr = sessionStorage.getItem("lastReq");
+    let lastResStr = sessionStorage.getItem("lastRes");
+
+    console.log("lastReq:\n", lastReqStr);
+    console.log("nowReq:\n", bodyStr);
+    // same as last submit or no another submit
+    if (bodyStr !== lastReqStr) {
       post('http://127.0.0.1:8000/query/', body)
         .then(data => {
           console.log(data);
           setRes(data);
+          sessionStorage.setItem("lastReq", bodyStr);
+          sessionStorage.setItem("lastRes", JSON.stringify(data));
         })
         .catch((err) => { console.log(err) });
+    }
+    else {
+      setRes(JSON.parse(lastResStr));
     }
   }, []);
 
   return (
     <>
       <span>
-        {res}
+        {JSON.stringify(res)}
       </span>
     </>
   );
