4fd4ec9f124c211b83d48fd8dbbb0c961cae1ad5
UqoAoqU <2932779705@qq.com>
2023-07-21 19:35:51 +0800

    add csrf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,8 +30,7 @@ ALLOWED_HOSTS = [
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",
     "http://localhost:3000",
@@ -40,14 +39,17 @@ CORS_ALLOWED_ORIGINS = [
     "http://0.0.0.0",
     "http://54.221.196.142",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://127.0.0.1",
-    "http://127.0.0.1:3000",
-    "http://0.0.0.0",
-    "http://54.221.196.142",
-]
+SESSION_COOKIE_SAMESITE = 'None'
+# CSRF_COOKIE_SAMESITE = 'None'
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost",
+#     "http://localhost:3000",
+#     "http://127.0.0.1",
+#     "http://127.0.0.1:3000",
+#     "http://0.0.0.0",
+#     "http://54.221.196.142",
+# ]
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,7 +71,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    #"django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
