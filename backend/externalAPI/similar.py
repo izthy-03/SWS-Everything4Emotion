f7d1a71593d2b51972bdc8be1243506b42dfe0af
UqoAoqU <2932779705@qq.com>
2023-07-24 00:25:15 +0800

    add similar_song recommedations

---
+++ backend/externalAPI/similar.py
@@ -0,0 +1,24 @@
+import json
+
+
+class similar:
+    def __init__(self, jsonfilename) -> None:
+        with open(jsonfilename, "rb") as jsonfile:
+            self.data = json.load(jsonfile)
+            self.map = {}
+            # make a map
+            for i, song in enumerate(self.data):
+                self.map[song['song_name']] = i
+            
+    def display(self) -> None:
+        print(type(self.data))   
+    def match(self, song_name:str) -> list:
+        ret = []
+        if (self.map.get(song_name)):
+            song_id = self.map[song_name]
+            for similar_song in self.data[song_id]['similar_songs']:
+               ret.append(similar_song['song_name'])
+        return ret 
+test = similar("similar_songs_details.json")
+while (1):
+    print(test.match(input()))
